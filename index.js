5eb366226c381c087be6432e6b1c1a6c4e63ae79
santiago <sarmiento.santiagonahuel@iesconcepcion.edu.ar>
2025-06-12 19:32:00 -0300

    p1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const a = document.getElementById("a"); // campo donde escribís
-    const b = document.getElementById("b"); // zona donde se muestra
-    a.addEventListener("input", () => b.textContent = a.value);
-  });
-  
-  
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+
+a.addEventListener("input", () => {
+  b.textContent = a.value;
+});
